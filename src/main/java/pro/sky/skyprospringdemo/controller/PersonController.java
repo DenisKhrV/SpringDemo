46056661f0d4f635a4b277e8ab07407ac61f9ed8
DenisKhrV <DenisKhr@yandex.ru>
2023-10-04 18:25:11 +1000

    1

--- src/main/java/pro/sky/skyprospringdemo/controller/PersonController.java
+++ src/main/java/pro/sky/skyprospringdemo/controller/PersonController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.skyprospringdemo.domain.Person;
-import pro.sky.skyprospringdemo.exceptions.BadPersonNumberException;
 import pro.sky.skyprospringdemo.service.PersonService;
 
 @RestController
@@ -20,6 +19,10 @@ public class PersonController {
         final String person;
         person = personService.getPerson(number);
         return person;
+    }
+    @GetMapping(path = "/person/by-passport")
+    public String getPersonInfo(@RequestParam("passport") String passport) {
+        return personService.getPersonByPassport(passport);
     }
     @GetMapping(path = "/person/add")
     public String addPerson(@RequestParam("name") String name,
